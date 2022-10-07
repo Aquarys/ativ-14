1c9a36a5bd56fc41c25b7b0bb6976ef8edf61a27
Alexandre Adryan <115313233+Aquarys@users.noreply.github.com>
2022-10-07 20:15:02 +0000

    ADICIONEI UM PRINT

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from conversor import*
 cem = float(input('digite um valor em centimetros que voce quer converter: '))
 pol(cem)
+print('querido diario hj aprendi a fazer isso aq q n sei o nome e espero q isso n me de dor de cabeça futuramente AMEM')
